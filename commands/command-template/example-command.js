71bc293284987fc544670e8842a0ed8aadab8013
Zach Neeley <54543335+zneeley@users.noreply.github.com>
2020-11-24 10:15:01 -0700

    Added commands template, general comments and remove ?messages  (#4)
    * Fixed configuratorEmbed
    
    * Added missing commands for guide, permissions, and server configs
    
    * Added removing of ?commands, added info to readme and added example cmd
    
    * Fixed Comments
    
    Co-authored-by: Brett Saunders <saunders.brett@outlook.com>

---
+++ commands/command-template/example-command.js
@@ -0,0 +1,84 @@
+// Imports the discord.js-commando library which is required for commands to work. 
+const { Command } = require('discord.js-commando');
+
+/* Define the contents and structure of embedded message for bot response.
+Colour Codes for embed messages are in decimal format, see bellow website for help:
+https://leovoel.github.io/embed-visualizer/ 
+https://convertingcolors.com/ */
+const exampleEmbed = {
+	/* This is the main body of the embedded message. The \n character will
+	add new lines for extra space in the command. */
+	"description": "This is the Craftory Bot command template. If you are wanting to help improve the bot learn more here. \n\nNOTE: This command will be removed in 5 seconds! \n\nhttps://github.com/CraftoryStudios/DiscordBot",
+	
+	// The color of the embed is in a decimal format.
+	"color": 3077393,
+	
+	// The author is what the smaller icon and name is under.
+	"author": {
+		
+		/* name - Is what is reported when the command is issued.
+				  The name much be what the command is about. 
+				  
+				  For example: Becasue this is the example command the name will be Example Command. */
+		"name": "Example Command",
+		
+		/* icon_url - Is the small icon that appears next to the name field. To change
+					  just replace the the link below with the direct image url. 
+					  
+					  NOTE: The url must end with the image file extension. */
+		"icon_url": "https://cdn.discordapp.com/attachments/731820462119714828/780132122055933952/icon.png"
+	},
+	
+	// Thumbnails are the larger image to the right of the embed body.
+	"thumbnail": {
+		
+		/* url - Will be the direct link to the image. It must be related
+		         to the command. Which a good default for commands that 
+		         dont have a direct image like the plugins permissions you can
+		         just use the image from the spigot page. 
+				 
+		    	 NOTE: The url must end with the image file extension. */
+		"url": "https://www.spigotmc.org/data/resource_icons/81/81151.jpg"
+	}
+};
+
+// Create the command by calling command's client.
+// NOTE: All command fields must be in lower case!
+module.exports = class WikiLink extends Command {
+	constructor(client) {
+		super(client, {
+			/* name - Command called in discord messages. 
+					  For example: ?example-command is how a user will issue this command. */
+			name: 'example-command',
+			
+			/* aliases - Can be set to a shorter version of the commands name
+						 For example: ?excmd will issue ?example-command with a shorter name. */
+			aliases: ['excmd'],
+			
+			/* group - Needs to be the same as parent folders name.
+					   For example: This command is named command-template so the group needs
+									to be command-template. */
+			group: 'command-template',
+			
+			/* memberName - Is just the name of the command when listing the commands
+							in a group. Recommend to just set to the same name as the
+							command. */
+			memberName: 'resource-pack',
+			
+			/* description - This is what is returned when asking the bot to return all
+							 commands. Can be something simple or very detailed. */
+			description: 'Replies with a link to the resource pack.'
+		});
+	}
+
+	// Once the command is issued this will make the bot return the needed message
+	run(message) {
+		
+		/* Returns the message calling exampleEmbed to have the message with an embed.
+					NOTE: You will only need to have:
+						  return message.say({ embed: exampleEmbed }); 
+						  The .then part after the message.say is only for this templete to auto
+						  remove the embed because this is not a real command. */
+		return message.say({ embed: exampleEmbed }).then(msg => {msg.delete({ timeout: 5000 }) });
+	}
+};
--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const { CommandoClient } = require('discord.js-commando');
 const path = require('path');
 const admin = require("firebase-admin");
-const express = require('express');
-const request = require('request');
-const bodyParser = require('body-parser');
-
-var app = express();
-var port = process.env.PORT || 3123;
-
 
 // --Database Connection--
 admin.initializeApp({
@@ -15,17 +8,22 @@ admin.initializeApp({
 	databaseURL: "https://discord-bot-d78e8.firebaseio.com"
   });
 
-// --Discord Bot--
-
+/* Set up the bot to report on the craftory discord and with the ? as a
+listener for commands. */
 const client = new CommandoClient({
 	commandPrefix: '?',
 	owner: '197773056557580288',
 	invite: 'https://discord.gg/bRCvFy9',
 });
 
+/* Register any needed groups for the commands
+   NOTE: If you add any folder with commands make sure to
+   add the new folder here and a decent description.*/
 client.registry
 	.registerDefaultTypes()
 	.registerGroups([
+		// foldername, description
+		['command-template','Example command to help with new command creation'],
 		['wiki', 'Commands related to wiki actions'],
 		['wiki-machines', 'Commands related to machines on the wiki'],
 		['wiki-ores', 'Commands related to ores on the wiki'],
@@ -35,31 +33,24 @@ client.registry
 	.registerDefaultGroups()
 	.registerDefaultCommands()
     .registerCommandsIn(path.join(__dirname, 'commands'));
-    
+
+/* Once the bot is initialized report in console and set the bots
+   activity as needed. */
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
     client.user.setActivity('Crafting Craftory');
 });
 
+// Read all incoming messages and remove any message needed
+client.on('message', message => {
+	// Remove messages that start with ? after 1 second
+	if (message.content.startsWith("?")) {
+		message.delete({timeout: 1000});
+    }
+});
+
+// Write any errors to console
 client.on('error', console.error);
 
+// Login to the discords using a env 
 client.login(process.env.token);
-
-// --Webhooks Trello--
-
-// Allows us to easily read the payload from the webhook
-app.use( bodyParser.json() );
-
-app.all("/trello", function(req, res, next) {
-	console.log(req.body);
-	res.send('OK');
-});
-
-
-// Standard NodeJS Listener
-var server = app.listen(port, function () {
-	var host = server.address().address;
-	var port = server.address().port;
-
-	console.log('Listening at http://%s:%s in %s', host, port);
-});
