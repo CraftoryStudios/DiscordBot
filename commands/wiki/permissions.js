2814b113edb5bb68b058d313c83d98fbac571c56
Zach Neeley <54543335+zneeley98@users.noreply.github.com>
2020-11-23 17:43:16 -0700

    Added missing permissions, guide and server config cmds (#2)
    * Fixed configuratorEmbed
    
    * Added missing commands for guide, permissions, and server configs

---
+++ commands/wiki/permissions.js
@@ -0,0 +1,29 @@
+const { Command } = require('discord.js-commando');
+
+const permissionsEmbed = {
+  "description": "Here is a list of all current permissions that craftory has to offer. \n\nhttps://craftory.studio/craftory-tech/wiki/permissions.html",
+  "color": 8156547,
+  "author": {
+    "name": "Permissions",
+    "icon_url": "https://cdn.discordapp.com/attachments/731820462119714828/780132122055933952/icon.png"
+  },
+  "thumbnail": {
+    "url": "https://www.spigotmc.org/data/resource_icons/81/81151.jpg?1594072221"
+  }
+};
+
+module.exports = class WikiLink extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'permissions',
+			aliases: ['perms'],
+			group: 'wiki',
+			memberName: 'permissions',
+			description: 'Replies with a link to the permissions page.'
+		});
+	}
+
+	run(message) {
+		return message.say({ embed: permissionsEmbed });
+	}
+};
