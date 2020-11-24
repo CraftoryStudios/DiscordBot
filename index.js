945e5c0e8a6b40250f701bf58313a72710f3bb11
Zach <zneeley@nmsu.edu>
2020-11-24 09:54:02 -0700

    Fixed Comments

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const { CommandoClient } = require('discord.js-commando');
 const path = require('path');
+const admin = require("firebase-admin");
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
 
-// Set up the bot to report on the craftory discord and with the ? as a
-// listener for commands. 
+var app = express();
+var port = process.env.PORT || 3123;
+
+// --Database Connection--
+admin.initializeApp({
+	credential: admin.credential.cert(JSON.parse(Buffer.from(process.env.FIREBASE_BASE64, 'base64').toString('ascii'))),
+	databaseURL: "https://discord-bot-d78e8.firebaseio.com"
+});
+
+
+/* Set up the bot to report on the craftory discord and with the ? as a
+listener for commands. */
 const client = new CommandoClient({
 	commandPrefix: '?',
 	owner: '197773056557580288',
 	invite: 'https://discord.gg/bRCvFy9',
 });
 
-// Register any needed groups for the commands
-// NOTE: If you add any folder with commands make sure to
-//		 add the new folder here and a decent description.
+/* Register any needed groups for the commands
+   NOTE: If you add any folder with commands make sure to
+   add the new folder here and a decent description.*/
 client.registry
 	.registerDefaultTypes()
 	.registerGroups([
@@ -27,8 +41,8 @@ client.registry
 	.registerDefaultCommands()
     .registerCommandsIn(path.join(__dirname, 'commands'));
 
-// Once the bot is initialized report in console and set the bots
-// activity as needed. 
+/* Once the bot is initialized report in console and set the bots
+   activity as needed. */
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
     client.user.setActivity('Crafting Craftory');
@@ -47,3 +61,22 @@ client.on('error', console.error);
 
 // Login to the discords using a env 
 client.login(process.env.token);
+
+// --Webhooks Trello--
+
+// Allows us to easily read the payload from the webhook
+app.use( bodyParser.json() );
+
+app.all("/trello", function(req, res, next) {
+	console.log(req.body);
+	res.send('OK');
+});
+
+
+// Standard NodeJS Listener
+var server = app.listen(port, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Listening at http://%s:%s in %s', host, port);
+});
