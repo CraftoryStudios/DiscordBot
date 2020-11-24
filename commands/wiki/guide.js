f6129d81816b9e21fbe3668576a9ef758be96a9e
Zach <zneeley@nmsu.edu>
2020-11-23 17:31:09 -0700

    Added missing commands for guide, permissions, and server configs

---
+++ commands/wiki/guide.js
@@ -0,0 +1,29 @@
+const { Command } = require('discord.js-commando');
+
+const guideEmbed = {
+  "description": "Need help getting started or just needing to look catch up on the plugin. Here is the official Craftory guide.\n\nhttps://craftory.studio/craftory-tech/wiki/getting-started.html",
+  "color": 12314511,
+  "author": {
+    "name": "Getting Started",
+    "icon_url": "https://cdn.discordapp.com/attachments/731820462119714828/780132122055933952/icon.png"
+  },
+  "thumbnail": {
+    "url": "https://www.spigotmc.org/data/resource_icons/81/81151.jpg?1594072221"
+  }
+};
+
+module.exports = class WikiLink extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'guide',
+			aliases: ['guide'],
+			group: 'wiki',
+			memberName: 'guide',
+			description: 'Replies with a link to the getting stated page.'
+		});
+	}
+
+	run(message) {
+		return message.say({ embed: guideEmbed });
+	}
+};
---
+++ commands/wiki/server-config.js
@@ -0,0 +1,29 @@
+const { Command } = require('discord.js-commando');
+
+const configEmded = {
+  "description": "You can change several things when looking at the Craftory config file. For example changing the plugins language. Learn more here!\n\nhttps://craftory.studio/craftory-tech/wiki/server-config.html",
+  "color": 10579918,
+  "author": {
+    "name": "Server Config",
+    "icon_url": "https://cdn.discordapp.com/attachments/731820462119714828/780132122055933952/icon.png"
+  },
+  "thumbnail": {
+    "url": "https://www.spigotmc.org/data/resource_icons/81/81151.jpg?1594072221"
+  }
+};
+
+module.exports = class WikiLink extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'server-config',
+			aliases: ['servconfig'],
+			group: 'wiki',
+			memberName: 'server-config',
+			description: 'Replies with a link to the server config page.'
+		});
+	}
+
+	run(message) {
+		return message.say({ embed: configEmded });
+	}
+};
