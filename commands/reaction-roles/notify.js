ae79f7bf4a262eab5881176e6570f6fb4bea9670
SizzlinSeal <67880368+SizzlinSeal@users.noreply.github.com>
2020-12-14 13:55:34 -0800

    added embed

--- commands/reaction-roles/notify.js
+++ commands/reaction-roles/notify.js
@@ -1,5 +1,16 @@
 // Imports the discord.js-commando library which is required for commands to work. 
 const { Command } = require('discord.js-commando');
+const channel = client.channels.cache.get('732161595362377819');
+
+const Embed = {
+	/* This is the main body of the embedded message. The \n character will
+	add new lines for extra space in the command. */
+	"description": "React to this message to get the Notify role. \n\nReact to this message to get the Notify role. \n\nWe will still Notify all of important updates, but this role will notify you on release of new DevLogs and additional resources!",
+	
+	// The color of the embed is in a decimal format.
+	"color": 3077393,
+};
+
 
 client.on("message", async message => {
     // Checking if the message author is a bot.
@@ -11,10 +22,6 @@ client.on("message", async message => {
     // Creating a filter.
     const Filter = (reaction, user) => user.id == message.author.id;
 
-    // Creating the embed message.
-    const Embed = new discord.MessageEmbed()
-        .setDescription(`Choose a role: ${Role1.name}`)
-    
     // Awaiting for the embed message to be sent.
     const reactionMessage = await message.channel.send(Embed);
 
@@ -22,7 +29,7 @@ client.on("message", async message => {
     await reactionMessage.react("📢");
 
     // Awaiting a reaction to the embed message. Time is measured in ms. (30000 ms - 30 seconds)
-    reactionMessage.awaitReactions(Filter, {max: 1, time: 30000, errors: ["time"]}).then(collected => {
+    reactionMessage.awaitReactions(Filter, {max: 1, time: 3000, errors: ["time"]}).then(collected => {
         // Getting the first reaction in the collection.
         const reaction = collected.first();
         
@@ -30,9 +37,9 @@ client.on("message", async message => {
         switch (reaction.emoji.name) {
             case "📢":
                 // Checking if the member already has the role.
-                if (message.member.roles.cache.has(Role1.id)) {return message.channel.send("You already have the role.")};
+                if (message.member.roles.cache.has(Role1.id)) {return user.send("You already subscribed to the Notify role in Craftory")};
                 // Adding the role.
-                message.member.roles.add(Role1).then(message.channel.send("Role added!"));
+                message.member.roles.add(Role1).then(user.send("You are now subscribed to Craftory devlogs!"));
                 // Breaking the switch statement to make sure no other cases are executed.
                 break
         }
